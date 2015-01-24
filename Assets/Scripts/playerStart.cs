be743f6a94c6b283b45a3662f3d3f989bc2e443e
Douglas Wong <fight320@hotmail.com>
2015-01-24 04:30:02 -0800

    added menu scene

--- Assets/Scripts/playerStart.cs
+++ Assets/Scripts/playerStart.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerStart : MonoBehaviour {
+public class PlayerStart : MonoBehaviour {
 	public bool houseInteract = false;
 	public bool grounded = false;
 	private Animator anim;
 	public bool gameStarted = false;
-
+	public float speed = 10f;
+	public float timeLeft = 30.0f;
 //	void Awake(){
 //		anim = GetComponent<Animator>();
 //	}
@@ -23,36 +24,35 @@ public class playerStart : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetButtonDown ("Forward")){
-			if((grounded == true) && (gameStarted == true)){
-				Vector3 newpos = new Vector3(0f,0f,0.3f);
-				rigidbody.MovePosition(rigidbody.position - newpos);
-			}
-		}
-		if(Input.GetButtonDown ("Backward")){
-			if((grounded == true) && (gameStarted == true)){
-				Vector3 newpos = new Vector3(0f,0f,0.3f);
-				rigidbody.MovePosition(rigidbody.position + newpos);
-			}
-		}
-		if(Input.GetButtonDown ("Left")){
-			if((grounded == true) && (gameStarted == true)){
-				Vector3 newpos = new Vector3(0.3f,0f,0f);
-				rigidbody.MovePosition(rigidbody.position - newpos);
-			}
-		}
-		if(Input.GetButtonDown ("Right")){
-			if((grounded == true) && (gameStarted == true)){
-				Vector3 newpos = new Vector3(0.3f,0f,0f);
-				rigidbody.MovePosition(rigidbody.position + newpos);
-			}
-		}
-//		if(Input.GetButtonDown ("Horizontal")){
-//			if(grounded == true) && (gameStarted == true)){
-//				Vector3 newpos = new Vector3(0,0,1);
-//				rigidbody.MovePosition(rigidbody.position - newpos);
-//			}
+//		Check whether time is up
+//		timeLeft -= Time.deltaTime;
+//		if(timeLeft < 0){
+//			Switch();
 //		}
+
+		if(Input.GetButton("Forward")){
+			if((grounded == true) && (gameStarted == true)){
+				rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+			}
+		}
+
+		if(Input.GetButton("Backward")){
+			if((grounded == true) && (gameStarted == true)){
+				rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+			}
+		}
+
+		if(Input.GetButton("Right")){
+			if((grounded == true) && (gameStarted == true)){
+				rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+			}
+		}
+
+		if(Input.GetButton("Left")){
+			if((grounded == true) && (gameStarted == true)){
+				rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+			}
+		}
 	}
 
 }
